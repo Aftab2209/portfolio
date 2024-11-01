354f93c27093715abec4a574ea6d7c3a4e510d1e
Aftab <aftabalam2209@gmail.com>
2024-11-01 22:42:31 +0530

    lamp fix

--- components/ui/lamp.jsx
+++ components/ui/lamp.jsx
@@ -17,7 +17,7 @@ export function LampDemo() {
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
         Hi, I’m Aftab
-        <div className="text-xl md:text-2xl lg:text-xl w-[100%] mt-2 flex items-center justify-center">
+        <div className="text-lg md:text-2xl lg:text-xl w-[100%] mt-2 flex items-center justify-center">
           <p className="w-[80%] text-wrap text-center">
             a full-stack developer passionate about creating end-to-end web
             solutions. With expertise across Angular, React, Node.js, and
@@ -42,7 +42,7 @@ export const LampContainer = ({ children, className }) => {
       <div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0 ">
         <motion.div
           initial={{ opacity: 0.5, width: "15rem" }}
-          whileInView={{ opacity: 1, width: "33%" }}
+          whileInView={{ width: window?.innerWidth < 1000 ? '100%' : '33%' }} 
           transition={{
             delay: 0.3,
             duration: 0.8,
