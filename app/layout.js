dfbc49e269d0ff8e4a54c5bab4dfc604ff117f3c
Aftab <aftabalam2209@gmail.com>
2024-11-03 15:04:08 +0530

    Analytics Added.

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react"
 import { Poppins } from 'next/font/google';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
         className={`${poppins.className} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
