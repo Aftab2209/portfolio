0348ddfc52e20eb6219fb411e47b71484dbf6f95
Aftab <aftabalam2209@gmail.com>
2024-11-03 12:49:29 +0530

    compressed image

--- components/ui/time.jsx
+++ components/ui/time.jsx
@@ -35,7 +35,7 @@ export function TimelineDemo() {
 
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="/hack-com.JPG"
+              src="/hack-com.jpg"
               alt="startup template"
               width={500}
               height={500}
