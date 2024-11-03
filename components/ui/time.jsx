82380fc0beee804a0953a660af9b3d5cfb2e233d
Aftab <aftabalam2209@gmail.com>
2024-11-03 12:33:03 +0530

    button mobile fix

--- components/ui/time.jsx
+++ components/ui/time.jsx
@@ -15,7 +15,7 @@ export function TimelineDemo() {
             pickups, and more. It was an inspiring collaboration and a rewarding
             learning experience!
           </p>
-          <div className="flex gap-2 mb-2">
+          <div className="flex gap-2 mb-2 flex-wrap">
             <button className="rounded-2xl inline-flex h-8 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-3 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400">
               Google Maps API
             </button>
